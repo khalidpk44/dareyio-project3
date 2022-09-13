296c28ebe6fa8c29842d3e7dabb35573e95f7a83
khalidpk44 <100615007+khalidpk44@users.noreply.github.com>
2022-09-13 22:20:50 +0100

    Update project3.sh

--- project3.sh
+++ project3.sh
@@ -4,10 +4,13 @@ curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 mkdir Todo
 mkdir routes
-
+mkdir models
 cp ./index.js ./Todo/
+cp ./api.js ./routes/
+cp ./todo.js ./models/
 cd Todo
 npm init
 npm install express
 npm install dotenv
+npm install mongoose
 node index.js
