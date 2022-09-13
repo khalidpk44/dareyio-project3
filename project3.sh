ff783d6b088dd917cfbfa3c73af96b7322b0621d
khalidpk44 <100615007+khalidpk44@users.noreply.github.com>
2022-09-13 22:16:44 +0100

    Update project3.sh

--- project3.sh
+++ project3.sh
@@ -3,6 +3,8 @@ sudo apt upgrade
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 mkdir Todo
+mkdir routes
+
 cp ./index.js ./Todo/
 cd Todo
 npm init
